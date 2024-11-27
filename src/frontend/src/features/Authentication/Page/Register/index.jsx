3bc4ba8b5fd90577937de25034cb8a982106cd03
betapdi <lecongquochan@gmail.com>
2024-11-27 07:57:23 +0700

    Fix tailwind

--- src/frontend/src/features/Authentication/Page/Register/index.jsx
+++ src/frontend/src/features/Authentication/Page/Register/index.jsx
@@ -2,10 +2,10 @@ import * as React from "react";
 
 export default function SignUpPage() {
   return (
-    <div className="w-[1920px] h-[100px] pl-[300px] pr-[1494px] py-[30px] justify-start items-center inline-flex">
-      <div className="flex items-center gap-2">
-      <img src={require("../../../../image/logo_gg.png")} alt="logo" className="h-auto max-w-full" />
-        <div className="text-[#18191c] text-2xl font-inter">Antise</div>
+    <div className="w-[1920px] h-[100px] pl-[300px] pr-[1494px] py-[30px] justify-start items-center inline-flex border">
+      <div className="flex items-center gap-2 border">
+        <img src={require("../../../../image/logo_gg.png")} alt="logo" className="h-auto max-w-full border" />
+        <div className="text-[#18191c] text-2xl font-inter border">Antise</div>
       </div>
     </div>
   );
