bf69f1bd48c92d8aebb159f09808a41ceec14798
Huynh Phan Nhat Vy <vyhuynh1108@gmail.com>
2024-11-29 01:36:41 +0700

    completed sign up UI

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      'blue': '#0A65CC',
+      'black': '#18191C',
+      'gray': '#5E6670',
+      'white': '#FFFFFF',
+      'gray/100': '#E4E5E8'
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'], 
+    },
   },
   plugins: [],
 }
