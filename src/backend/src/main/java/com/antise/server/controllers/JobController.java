66b534c99ff15ab5229ebc3f8ac364fef758ec57
BeTapDi <lecongquochan@gmail.com>
2024-12-01 23:38:16 +0700

    add default action

--- src/backend/src/main/java/com/antise/server/controllers/JobController.java
+++ src/backend/src/main/java/com/antise/server/controllers/JobController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.antise.server.dto.JobDto;
+import com.antise.server.services.JobService;
+
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
@@ -16,10 +19,19 @@ import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("api/v1/job")
 @AllArgsConstructor
 public class JobController {
+    private final JobService jobService;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<JobDto>> getAllJobs(@RequestParam String param) {
+        return ResponseEntity.ok(jobService.getAllJobs());
+    }
+ 
     
 }
--- src/backend/src/main/java/com/antise/server/services/JobService.java
+++ src/backend/src/main/java/com/antise/server/services/JobService.java
@@ -1,5 +1,43 @@
 package com.antise.server.services;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.antise.server.dto.JobDto;
+import com.antise.server.entities.Job;
+import com.antise.server.repositories.JobRepository;
+
+@Service
 public class JobService {
+    private final JobRepository jobRepository;
+    private final FileService fileService;
+
+    @Value("${project.static}")
+    private String path;
     
+    @Value("${base.url}")
+    private String baseUrl;
+
+    public JobService(JobRepository jobRepository, FileService fileService) {
+        this.jobRepository = jobRepository;
+        this.fileService = fileService;
+    }
+    
+    public List<JobDto> getAllJobs() {
+        List<Job> jobs = jobRepository.findAll();
+        List<JobDto> jobDtos = new ArrayList<>();
+
+        for (Job item : jobs) {
+            JobDto dto = new JobDto();
+            dto.update(item);
+            jobDtos.add(dto);
+        }
+
+        return jobDtos;
+    }
 }
