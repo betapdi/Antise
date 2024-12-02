d35c40823893db85204230a9e3e78d35eee4194b
pupu5003 <pnphuonguyen3005@gmail.com>
2024-12-03 00:27:41 +0700

    Detail Job page

--- src/frontend/src/features/JobSeeking/index.jsx
+++ src/frontend/src/features/JobSeeking/index.jsx
@@ -6,16 +6,19 @@ import MainPage from './pages/Main';
 import ProfilePage from "./pages/Profile";
 import HomePage from './pages/HomePage';
 import Header from '../../components/Header';
+import DetailJob from './pages/DetailJob';
+
 const Manga = (props) => {
   return (
     <div>
-      <Header/>
+      <Header />
       <Routes>
-        <Route exact path = '/' element = {<Navigate to = "/temp" replace />} />
-        <Route path = "/temp" element = {<MainPage/>} />
-        <Route path = "/profile" element = {<ProfilePage/>} />
-        <Route path = "/homePage" element = {<HomePage/>} />
-        <Route path = "*" element = {<p>NONE</p>} />
+        <Route exact path='/' element={<Navigate to="/temp" replace />} />
+        <Route path="/temp" element={<MainPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/DetailJob" element={<DetailJob />} />
+        <Route path="*" element={<p>NONE</p>} />
       </Routes>
     </div>
   )
