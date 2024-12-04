f56ec6dd4ebd06922459e5b93958dac08539bf12
Huynh Phan Nhat Vy <vyhuynh1108@gmail.com>
2024-12-05 00:47:57 +0700

    fix setting

--- src/frontend/src/features/JobSeeking/index.jsx
+++ src/frontend/src/features/JobSeeking/index.jsx
@@ -3,11 +3,11 @@ import {
   Route, Routes, Navigate
 } from "react-router-dom";
 import MainPage from './pages/Main';
-import ProfilePage from "./pages/Profile";
 import HomePage from './pages/HomePage';
 import Header from '../../components/Header';
 import DetailJob from './pages/DetailJob';
 import ListJob from './pages/ListJob';
+import Dashboard from './pages/Dashboard';
 const Manga = (props) => {
   return (
     <div>
@@ -15,15 +15,15 @@ const Manga = (props) => {
       <Routes>
         <Route exact path='/' element={<Navigate to="/temp" replace />} />
         <Route path="/temp" element={<MainPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/homePage" element={<HomePage />} />
-        <Route path="/DetailJob" element={<DetailJob />} />
-        <Route path="/ListJob" element={<ListJob isSearch ={0} />} />
-        <Route path="/ListJob/search" element={<ListJob isSearch ={1} />} />
+        <Route path="/detailjob" element={<DetailJob />} />
+        <Route path="/listjob" element={<ListJob isSearch ={0} />} />
+        <Route path="/listjob/search" element={<ListJob isSearch ={1} />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<p>NONE</p>} />
       </Routes>
     </div>
   );
 };
 
-export default App;
+export default Manga;
--- src/frontend/src/features/JobSeeking/pages/Dashboard/index.jsx
+++ src/frontend/src/features/JobSeeking/pages/Dashboard/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Sidebar from "../Component/SideBar";
-import Settings from "../Component/Settings";
+import Settings from './Component/Settings';
+import Sidebar from './Component/SideBar';
 
 const Dashboard = () => {
-
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center">
-      <div className="flex w-full max-w-screen-xl">
-        {/* Sidebar Component */}
-        <Sidebar />
-
-        {/* Main Content */}
-        <Settings />
+    <div className="w-screen flex justify-center">
+      <div className="w-[90%] max-w-screen-xl flex flex-row mt-5">
+        <div className="w-1/4 border-r border-[#adb5bd]">
+          <Sidebar />
+        </div>
+        <div className="w-3/4">
+          <Settings />
+        </div>
       </div>
     </div>
   );
