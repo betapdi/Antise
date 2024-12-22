884d79976a65741ff5ea2419470b435c28b386a3
pupu5003 <pnphuonguyen3005@gmail.com>
2024-12-22 11:40:31 +0700

    add, remove favorite job

--- src/frontend/src/api/applicantApi.js
+++ src/frontend/src/api/applicantApi.js
@@ -3,48 +3,65 @@ import axiosClient from "./axiosClient"
 import axiosPrivate from "./axiosPrivate"
 
 const applicantApi = {
-    createApplicantAccount: () => {
-        const url = '/applicant/create';
-        return axiosPrivate.post(url);
-    },
+  createApplicantAccount: () => {
+    const url = '/applicant/create';
+    return axiosPrivate.post(url);
+  },
 
-    editApplicant: (rawData) => {
-        const url = '/applicant/edit';
+  editApplicant: (rawData) => {
+    const url = '/applicant/edit';
 
-        const formData = new FormData();
-        const applicant = {};
+    const formData = new FormData();
+    const applicant = {};
 
-        for (let key in rawData) {
-          if (key === "resume") {
-            formData.append('resume', rawData[key]);
-          }
+    for (let key in rawData) {
+      if (key === "resume") {
+        formData.append('resume', rawData[key]);
+      }
 
-          else if (key === "profilePicture") {
-            formData.append('profileImage', rawData[key]);
-          }
+      else if (key === "profilePicture") {
+        formData.append('profileImage', rawData[key]);
+      }
 
-          else if (key === "gender") {
-            const value = parseInt(rawData[key]);
-            applicant[key] = value;
-          }
+      else if (key === "gender") {
+        const value = parseInt(rawData[key]);
+        applicant[key] = value;
+      }
 
-          else applicant[key] = rawData[key];
-        }
-      
-        formData.append('applicant', new Blob([JSON.stringify(applicant)], {type: 'application/json'}));
-      
-        // Send as multipart/form-data
-        return axiosPrivate.put(url, formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-    },
-
-    getResume: (fileName) => {
-      const url = fileName + "/details"
-      return axiosPrivate.get(url);
+      else applicant[key] = rawData[key];
     }
+
+    formData.append('applicant', new Blob([JSON.stringify(applicant)], { type: 'application/json' }));
+
+    // Send as multipart/form-data
+    return axiosPrivate.put(url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
+
+  getResume: (fileName) => {
+    const url = fileName + "/details"
+    return axiosPrivate.get(url);
+  },
+
+  addFavoriteJob: (id) => {
+    const url = `/applicant/addFavoriteJob/${id}`;
+    const formData = new FormData();
+    formData.append('job', new Blob([JSON.stringify(id)], { type: 'application/json' }));
+    return axiosPrivate.post(url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
+
+  removeFavoriteJob: (id) => {
+    const url = `/applicant/removeFavoriteJob/${id}`;
+    return axiosPrivate.delete(url);
+  },
+
 }
 
 export default applicantApi;
