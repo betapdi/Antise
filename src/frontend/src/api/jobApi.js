b6a840fa19c2e88295a8493a8449249f24caef38
pupu5003 <pnphuonguyen3005@gmail.com>
2024-12-23 13:40:14 +0700

    apply form post api step 1

--- src/frontend/src/api/jobApi.js
+++ src/frontend/src/api/jobApi.js
@@ -3,39 +3,49 @@ import axiosClient from "./axiosClient"
 import axiosPrivate from "./axiosPrivate"
 
 const jobApi = {
-    createJob: (rawData) => {
-        const url = '/job/create';
-      
-        // Convert rawData to FormData
-        const formData = new FormData();
-        formData.append('job', new Blob([JSON.stringify(rawData)], {type: 'application/json'}));
-      
-        // Send as multipart/form-data
-        return axiosPrivate.post(url, formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-    },
-    getAllJobs: () => {
-        const url = 'public/job/getAll';
-        return axiosClient.get(url);
-    },
+  createJob: (rawData) => {
+    const url = '/job/create';
 
-    getJob: (id) => {
-      const url = `public/job/get/${id}`;
-      return axiosClient.get(url);
-    },
-    searchJob: (searchData) => {
-      const url = 'public/job/search';
-      const formData = new FormData();  
-      formData.append('searchData', new Blob([JSON.stringify(searchData)], {type: 'application/json'}));
-      return axiosClient.post(url, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-    },
+    // Convert rawData to FormData
+    const formData = new FormData();
+    formData.append('job', new Blob([JSON.stringify(rawData)], { type: 'application/json' }));
+
+    // Send as multipart/form-data
+    return axiosPrivate.post(url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
+  getAllJobs: () => {
+    const url = 'public/job/getAll';
+    return axiosClient.get(url);
+  },
+
+  getJob: (id) => {
+    const url = `public/job/get/${id}`;
+    return axiosClient.get(url);
+  },
+
+  searchJob: (searchData) => {
+    const url = 'public/job/search';
+    const formData = new FormData();
+    formData.append('searchData', new Blob([JSON.stringify(searchData)], { type: 'application/json' }));
+    return axiosClient.post(url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
+
+  applyJob: (applyData) => {
+    const url = '/job/apply';
+    return axiosClient.post(url, applyData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
 }
 
 export default jobApi;
--- src/frontend/src/components/Form/applyform.jsx
+++ src/frontend/src/components/Form/applyform.jsx
@@ -7,6 +7,8 @@ import TextField from "../../customFields/TextField";
 import SelectField from "../../customFields/SelectField";
 import RichTextField from "../../customFields/RichTextField";
 import PopupDialog from "./PopupDialog";
+import jobApi from "../../api/jobApi";
+import { ApplicantContext } from "../../context/ApplicantContext";
 
 const ApplyForm = ({ isCloseChange, job }) => {
   const [isOpenDialog, setIsOpenDialog] = useState(false);
@@ -22,8 +24,20 @@ const ApplyForm = ({ isCloseChange, job }) => {
   }
 
   const phoneRegExp = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
+
   const handleClickSubmit = async (values) => {
     try {
+      const applyData = {
+        ...values,
+        jobId: job.id,
+        applicantId: ApplicantContext.applicant.id,
+      };
+
+      const formData = new FormData();
+      formData.append("applyData", new Blob([JSON.stringify(applyData)], { type: "application/json" }));
+      const response = await jobApi.applyJob(formData);
+      console.log("HHHH:", response);
+
       setDialogContent({
         title: "Submit Form Successfully",
         content: "Your form has been submitted!",
@@ -32,6 +46,7 @@ const ApplyForm = ({ isCloseChange, job }) => {
       });
     } catch (error) {
       console.error("Error submitting form:", error);
+
       setDialogContent({
         title: "Error",
         content: "There was an error submitting your form. Please try again later!",
@@ -47,115 +62,119 @@ const ApplyForm = ({ isCloseChange, job }) => {
     <div className="w-full mx-auto p-8 bg-white shadow-md rounded">
       <h1 className="text-[#18191c] mb-3 text-lg font-medium font-['Inter'] leading-7">{job.title}</h1>
       <div>
-      <Formik
-        initialValues={{
-          fullName: "",
-          phoneNumber: "",
-          email: "",
-          resume: "",
-          coverLetter: "",
-          questions: "",
-        }}
-        validationSchema={Yup.object({
-          fullName: Yup.string().required("Please fill your full name"),
-          phoneNumber: Yup.string().matches(phoneRegExp, "Please enter a valid phone number").required("Please fill your telephone number"),
-          email: Yup.string().email("Invalid email").required("Please fill your email"),
-          resume: Yup.string().required("Please upload your CV"),
-          coverLetter: Yup.string().required("Please fill the cover letter. If not, fill N/A"),
-          ...(job?.questions && {
-            questions: Yup.string().required("Please answer the questions"),
-          }),
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            //alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            handleClickSubmit(values);
-            //isCloseChange(true);
-          }, 400);
-        }}
-      >
-        {({ values, errors, touched, setFieldValue, isSubmitting }) => {
-          console.log("Formik errors:", errors);
-          return (
-            <Form className="space-y-4">
-              <Field
-                label="Full Name"
-                name="fullName"
-                component={TextField}
-                placeholder="Company Name"
-              />
+        <Formik
+          initialValues={{
+            fullName: "",
+            phoneNumber: "",
+            email: "",
+            resume: "",
+            coverLetter: "",
+            questions: "",
+          }}
+          validationSchema={Yup.object({
+            fullName: Yup.string().required("Please fill your full name"),
+            phoneNumber: Yup.string().matches(phoneRegExp, "Please enter a valid phone number").required("Please fill your telephone number"),
+            email: Yup.string().email("Invalid email").required("Please fill your email"),
+            resume: Yup.string().required("Please upload your CV"),
+            coverLetter: Yup.string().required("Please fill the cover letter. If not, fill N/A"),
+            ...(job?.questions && {
+              questions: Yup.string().required("Please answer the questions"),
+            }),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            const filteredValues = { ...values };
+            if (!job?.questions) {
+              delete filteredValues.questions;
+            }
 
-              <Field
-                label="Telephone Number"
-                name="phoneNumber"
-                component={TextField}
-                placeholder="Telephone number"
-              />
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+              handleClickSubmit(values);
+              //isCloseChange(true);
+            }, 400);
+          }}
+        >
+          {({ values, errors, touched, setFieldValue, isSubmitting }) => {
+            console.log("Formik errors:", errors);
+            return (
+              <Form className="space-y-4">
+                <Field
+                  label="Full Name"
+                  name="fullName"
+                  component={TextField}
+                  placeholder="Company Name"
+                />
 
-              <Field
-                label="Email"
-                name="email"
-                component={TextField}
-                placeholder="Email"
-              />
+                <Field
+                  label="Telephone Number"
+                  name="phoneNumber"
+                  component={TextField}
+                  placeholder="Telephone number"
+                />
 
-              <Field
-                name="resume"
-                component={SelectField}
-                label="Resume"
-                options={[{ key: "Select...", value: "" },
-                { key: "Resume 1", value: "Resume 1" },
-                { key: "Resume 2", value: "Resume 2" }]}
-              />
+                <Field
+                  label="Email"
+                  name="email"
+                  component={TextField}
+                  placeholder="Email"
+                />
 
-              <Field
-                label="Cover Letter"
-                name="coverLetter"
-                component={RichTextField}
-                placeholder="Cover Letter"
-              />
+                <Field
+                  name="resume"
+                  component={SelectField}
+                  label="Resume"
+                  options={[{ key: "Select...", value: "" },
+                  { key: "Resume", value: "Resume" }]}
+                />
 
-              {job?.questions && (
-                <div className="mt-4 ">
-                  <h3 className="text-sm font-medium text-[#18191c]"> Questions</h3>
-                  <div className="mb-2">
-                    {job.responsibility.split('\n').map((line, index) => (
-                      <div key={index} className="flex items-start">
-                        <span className="text-sm font-small text-[#18191c] px-1">•</span>
-                        <span>{line}</span>
-                      </div>
-                    ))}
+                <Field
+                  label="Cover Letter"
+                  name="coverLetter"
+                  component={RichTextField}
+                  placeholder="Cover Letter"
+                />
+
+                {job?.questions && (
+                  <div className="mt-4 ">
+                    <h3 className="text-sm font-medium text-[#18191c]"> Questions</h3>
+                    <div className="mb-2">
+                      {job.responsibility.split('\n').map((line, index) => (
+                        <div key={index} className="flex items-start">
+                          <span className="text-sm font-small text-[#18191c] px-1">•</span>
+                          <span>{line}</span>
+                        </div>
+                      ))}
+                    </div>
+                    <Field
+                      name="questions"
+                      component={RichTextField}
+                      placeholder="Questions"
+                    />
                   </div>
-                  <Field
-                    name="questions"
-                    component={RichTextField}
-                    placeholder="Questions"
-                  />
+                )}
+                <div className="flex flex-row justify-between">
+                  <button className="h-12 px-6 py-3 bg-[#e7f0fa] rounded-[3px] justify-center items-center gap-3 inline-flex"
+                    onClick={() => isCloseChange(true)}
+                  >
+                    <div className="text-[#0a65cc] text-base font-semibold font-['Inter'] capitalize leading-normal">Cancel</div>
+                  </button>
+                  <button
+                    type="submit"
+                    className="h-12 px-6 py-3 bg-[#0a65cc] rounded-[3px] justify-center items-center gap-3 inline-flex cursor-pointer"
+                  >
+                    <div className="text-white text-base font-semibold font-['Inter'] capitalize leading-normal">Apply Now</div>
+                    <img
+                      src={"/image/arrow_right_hover.png"}
+                      alt="arrow_right"
+                      className="h-4"
+                    />
+                  </button>
                 </div>
-              )}
-              <div className="flex flex-row justify-between">
-                <button className="h-12 px-6 py-3 bg-[#e7f0fa] rounded-[3px] justify-center items-center gap-3 inline-flex"
-                  onClick={() => isCloseChange(true)}
-                >
-                  <div className="text-[#0a65cc] text-base font-semibold font-['Inter'] capitalize leading-normal">Cancel</div>
-                </button>
-                <button
-                  type="submit"
-                  className="h-12 px-6 py-3 bg-[#0a65cc] rounded-[3px] justify-center items-center gap-3 inline-flex cursor-pointer"
-                >
-                  <div className="text-white text-base font-semibold font-['Inter'] capitalize leading-normal">Apply Now</div>
-                  <img
-                    src={"/image/arrow_right_hover.png"}
-                    alt="arrow_right"
-                    className="h-4"
-                  />
-                </button>
-              </div>
-            </Form>
-          )
-        }}
-      </Formik>
+              </Form>
+            )
+          }}
+        </Formik>
       </div>
       {isOpenDialog && (
         <PopupDialog
