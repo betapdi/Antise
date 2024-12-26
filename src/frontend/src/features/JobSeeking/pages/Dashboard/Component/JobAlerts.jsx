a5ef17771011d90d40af2b8a6f5129c56bf501b4
daobathanhh <118471153+daobathanhh@users.noreply.github.com>
2024-12-27 01:16:27 +0700

    Update JobAlerts.jsx

--- src/frontend/src/features/JobSeeking/pages/Dashboard/Component/JobAlerts.jsx
+++ src/frontend/src/features/JobSeeking/pages/Dashboard/Component/JobAlerts.jsx
@@ -1,110 +1,115 @@
 import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../../../../context/UserContext";
+import jobApi from "../../../../../api/jobApi";
 
 // lúc nào test fetch noti thì comment nguyên cái khúc notifications này (5-102) và bỏ dấu comment ở dòng 173
-const notifications = [
-    {
-        title: "Senior UX Designer",
-        companyLogo: "company_1.png",
-        contractType: "Contract Base",
-        location: "Australia",
-        salary: "$30K-$35K",
-        daysRemaining: 4,
-    },
-    {
-        title: "Software Engineer",
-        companyLogo: "company_2.png",
-        contractType: "Full Time",
-        location: "USA",
-        salary: "$50K-$70K",
-        daysRemaining: 7,
-    },
-    {
-        title: "Product Manager",
-        companyLogo: "company_3.png",
-        contractType: "Part Time",
-        location: "UK",
-        salary: "$40K-$50K",
-        daysRemaining: 10,
-    },
-    {
-        title: "Senior UX Designer",
-        companyLogo: "company_1.png",
-        contractType: "Contract Base",
-        location: "Australia",
-        salary: "$30K-$35K",
-        daysRemaining: 4,
-    },
-    {
-        title: "Software Engineer",
-        companyLogo: "company_2.png",
-        contractType: "Full Time",
-        location: "USA",
-        salary: "$50K-$70K",
-        daysRemaining: 7,
-    },
-    {
-        title: "Product Manager",
-        companyLogo: "company_3.png",
-        contractType: "Part Time",
-        location: "UK",
-        salary: "$40K-$50K",
-        daysRemaining: 10,
-    },
-    {
-        title: "Senior UX Designer",
-        companyLogo: "company_1.png",
-        contractType: "Contract Base",
-        location: "Australia",
-        salary: "$30K-$35K",
-        daysRemaining: 4,
-    },
-    {
-        title: "Software Engineer",
-        companyLogo: "company_2.png",
-        contractType: "Full Time",
-        location: "USA",
-        salary: "$50K-$70K",
-        daysRemaining: 7,
-    },
-    {
-        title: "Product Manager",
-        companyLogo: "company_3.png",
-        contractType: "Part Time",
-        location: "UK",
-        salary: "$40K-$50K",
-        daysRemaining: 10,
-    },
-    {
-        title: "Senior UX Designer",
-        companyLogo: "company_1.png",
-        contractType: "Contract Base",
-        location: "Australia",
-        salary: "$30K-$35K",
-        daysRemaining: 4,
-    },
-    {
-        title: "Software Engineer",
-        companyLogo: "company_2.png",
-        contractType: "Full Time",
-        location: "USA",
-        salary: "$50K-$70K",
-        daysRemaining: 7,
-    },
-    {
-        title: "Product Manager",
-        companyLogo: "company_3.png",
-        contractType: "Part Time",
-        location: "UK",
-        salary: "$40K-$50K",
-        daysRemaining: 10,
-    },
-];
+// const notifications = [
+//     {
+//         title: "Senior UX Designer",
+//         companyLogo: "company_1.png",
+//         contractType: "Contract Base",
+//         location: "Australia",
+//         salary: "$30K-$35K",
+//         daysRemaining: 4,
+//     },
+//     {
+//         title: "Software Engineer",
+//         companyLogo: "company_2.png",
+//         contractType: "Full Time",
+//         location: "USA",
+//         salary: "$50K-$70K",
+//         daysRemaining: 7,
+//     },
+//     {
+//         title: "Product Manager",
+//         companyLogo: "company_3.png",
+//         contractType: "Part Time",
+//         location: "UK",
+//         salary: "$40K-$50K",
+//         daysRemaining: 10,
+//     },
+//     {
+//         title: "Senior UX Designer",
+//         companyLogo: "company_1.png",
+//         contractType: "Contract Base",
+//         location: "Australia",
+//         salary: "$30K-$35K",
+//         daysRemaining: 4,
+//     },
+//     {
+//         title: "Software Engineer",
+//         companyLogo: "company_2.png",
+//         contractType: "Full Time",
+//         location: "USA",
+//         salary: "$50K-$70K",
+//         daysRemaining: 7,
+//     },
+//     {
+//         title: "Product Manager",
+//         companyLogo: "company_3.png",
+//         contractType: "Part Time",
+//         location: "UK",
+//         salary: "$40K-$50K",
+//         daysRemaining: 10,
+//     },
+//     {
+//         title: "Senior UX Designer",
+//         companyLogo: "company_1.png",
+//         contractType: "Contract Base",
+//         location: "Australia",
+//         salary: "$30K-$35K",
+//         daysRemaining: 4,
+//     },
+//     {
+//         title: "Software Engineer",
+//         companyLogo: "company_2.png",
+//         contractType: "Full Time",
+//         location: "USA",
+//         salary: "$50K-$70K",
+//         daysRemaining: 7,
+//     },
+//     {
+//         title: "Product Manager",
+//         companyLogo: "company_3.png",
+//         contractType: "Part Time",
+//         location: "UK",
+//         salary: "$40K-$50K",
+//         daysRemaining: 10,
+//     },
+//     {
+//         title: "Senior UX Designer",
+//         companyLogo: "company_1.png",
+//         contractType: "Contract Base",
+//         location: "Australia",
+//         salary: "$30K-$35K",
+//         daysRemaining: 4,
+//     },
+//     {
+//         title: "Software Engineer",
+//         companyLogo: "company_2.png",
+//         contractType: "Full Time",
+//         location: "USA",
+//         salary: "$50K-$70K",
+//         daysRemaining: 7,
+//     },
+//     {
+//         title: "Product Manager",
+//         companyLogo: "company_3.png",
+//         contractType: "Part Time",
+//         location: "UK",
+//         salary: "$40K-$50K",
+//         daysRemaining: 10,
+//     },
+// ];
 
 const ListJob = ({ jobs, numberOfJobs }) => (
   <div>
-    <span className="text-[#18191c] text-lg font-medium font-['Inter'] leading-7">Job Alerts </span>
-    <span className="text-[#9199a3] text-base font-normal font-['Inter'] leading-normal">({numberOfJobs} new jobs)</span>
+    <span className="text-[#18191c] text-lg font-medium font-['Inter'] leading-7">
+      Job Alerts
+    </span>
+    <span className="text-[#9199a3] text-base font-normal font-['Inter'] leading-normal">
+      ({numberOfJobs} new jobs)
+    </span>
     <div className="flex flex-col gap-3 items-center justify-center w-full mt-5">
       {jobs.map((job, index) => (
         <div
@@ -119,23 +124,33 @@ const ListJob = ({ jobs, numberOfJobs }) => (
             />
             <div className="flex-col justify-start items-start gap-3.5 inline-flex">
               <div className="justify-start items-center gap-2 inline-flex">
-                <div className="text-[#181f33] text-xl font-medium font-['Inter'] leading-loose">{job.title}</div>
+                <div className="text-[#181f33] text-xl font-medium font-['Inter'] leading-loose">
+                  {job.title}
+                </div>
                 <div className="px-3 py-[3px] bg-[#e8f1ff] rounded-[52px] justify-start items-start gap-2.5 flex">
-                  <div className="text-[#0a65cc] text-sm font-normal font-['Inter'] leading-tight">{job.contractType}</div>
+                  <div className="text-[#0a65cc] text-sm font-normal font-['Inter'] leading-tight">
+                    {job.contractType}
+                  </div>
                 </div>
               </div>
               <div className="justify-start items-center gap-4 inline-flex">
                 <div className="justify-start items-center gap-1.5 flex">
                   <img src={`/image/icon_map.png`} alt="location_icon" className="h-4" />
-                  <div className="text-[#636a7f] text-sm font-normal font-['Inter'] leading-tight">{job.location}</div>
+                  <div className="text-[#636a7f] text-sm font-normal font-['Inter'] leading-tight">
+                    {job.location}
+                  </div>
                 </div>
                 <div className="justify-start items-center gap-1 flex">
                   <img src={`/image/icon_salary.png`} alt="salary_icon" className="h-4" />
-                  <div className="text-[#636a7f] text-sm font-normal font-['Inter'] leading-tight">{job.salary}</div>
+                  <div className="text-[#636a7f] text-sm font-normal font-['Inter'] leading-tight">
+                    {job.minSalary}$ - {job.maxSalary}$
+                  </div>
                 </div>
                 <div className="justify-start items-center gap-1.5 flex">
                   <img src={`/image/icon_calander.png`} alt="calendar_icon" className="h-4" />
-                  <div className="text-[#636a7f] text-sm font-normal font-['Inter'] leading-tight">{job.daysRemaining} Days Remaining</div>
+                  <div className="text-[#636a7f] text-sm font-normal font-['Inter'] leading-tight">
+                    {job.daysRemaining} Days Remaining
+                  </div>
                 </div>
               </div>
             </div>
@@ -167,14 +182,47 @@ const ListJob = ({ jobs, numberOfJobs }) => (
 );
 
 const JobAlerts = ({ onJobCountChange }) => {
-  // lúc nào test ui page này mà khong fetch noti thì comment dòng 173 và bỏ dấu comment ở 5-102
-  // const { notifications } = useContext(UserContext);
+  const { notifications } = useContext(UserContext);
+  const [enrichedNotifications, setEnrichedNotifications] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
 
+  const calculateDaysRemaining = (expirationDate, postedDate) => {
+    const expiryDate = new Date(expirationDate);
+    const postDate = new Date(postedDate);
+  
+    const timeDiff = expiryDate - postDate;
+    const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+  
+    return daysRemaining > 0 ? daysRemaining : 0;
+  };
+  
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const promises = notifications.map(async (notification) => {
+          if (notification.jobId) {
+            const response = await jobApi.getJob(notification.jobId);
+            return { ...notification, ...response.data, daysRemaining: calculateDaysRemaining(response.data.expirationDate, response.data.postedDate), };
+          }
+          return notification;
+        });
+        const enrichedData = await Promise.all(promises);
+        setEnrichedNotifications(enrichedData);
+      } catch (error) {
+        console.error('Error fetching notifications:', error);
+      }
+    };
+
+    if (notifications?.length) {
+      fetchNotifications();
+    }
+  }, [notifications]);
+
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const paginatedJobs = (notifications || []).slice(startIndex, startIndex + itemsPerPage);
-  const totalPages = Math.ceil((notifications || []).length / itemsPerPage);
+  const paginatedJobs = enrichedNotifications.slice(startIndex, startIndex + itemsPerPage);
+  const totalPages = Math.ceil(enrichedNotifications.length / itemsPerPage);
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -190,14 +238,14 @@ const JobAlerts = ({ onJobCountChange }) => {
 
   useEffect(() => {
     if (onJobCountChange) {
-      onJobCountChange(notifications?.length || 0);
+      onJobCountChange(enrichedNotifications.length);
     }
-  }, [notifications, onJobCountChange]);
+  }, [enrichedNotifications, onJobCountChange]);
 
   return (
     <div>
       <div className="w-100 overflow-y-auto ml-8 mb-5">
-        <ListJob jobs={paginatedJobs} numberOfJobs={notifications?.length || 0} />
+        <ListJob jobs={paginatedJobs} numberOfJobs={enrichedNotifications.length} />
       </div>
       <div className="h-12 justify-center items-center gap-2 inline-flex w-full">
         <button
@@ -211,7 +259,9 @@ const JobAlerts = ({ onJobCountChange }) => {
         {Array.from({ length: totalPages }, (_, index) => (
           <button
             key={index}
-            className={`w-12 h-12 px-2 py-3 rounded-[50px] ${currentPage === index + 1 ? "bg-[#0a65cc] text-white" : "text-[#5e6670]"}`}
+            className={`w-12 h-12 px-2 py-3 rounded-[50px] ${
+              currentPage === index + 1 ? 'bg-[#0a65cc] text-white' : 'text-[#5e6670]'
+            }`}
             onClick={() => setCurrentPage(index + 1)}
           >
             {index + 1}
